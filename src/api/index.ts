35acfd0d6db513064b3c6bfb9416544b4578fbbe
shu <2061741422@qq.com>
2022-12-19 11:08:32 +0800

    add axios

---
+++ src/api/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
+
+const $api = axios.create({
+  baseURL: "",
+});
+
+$api.interceptors.request.use(
+  (config: AxiosRequestConfig): AxiosRequestConfig => {
+    // 发送请求前做些什么
+    return config;
+  },
+  (error) => {
+    // 对请求失败做些什么
+    return Promise.reject(error);
+  }
+);
+
+$api.interceptors.request.use(
+  (response) => {
+    // 对响应成功做些什么
+    return response;
+  },
+  (error) => {
+    // 对响应失败做些什么
+    return Promise.reject(error);
+  }
+);
+
+export default $api;
