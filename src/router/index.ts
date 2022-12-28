6a95ad5917a4c0686a894a1117b0acdaa20d2de1
shu <2061741422@qq.com>
2022-12-28 15:35:29 +0800

    the end

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
-import { useStore } from "vuex";
 import LayoutFrame from "components/layout/LayoutFrame.vue";
 import frameIn from "./frameIn";
 import frameOut from "./frameOut";
-
-const store = useStore();
 
 const routes: Array<RouteRecordRaw> = [
   {
